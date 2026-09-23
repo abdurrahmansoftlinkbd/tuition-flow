e4ffe89e381fd6cf6961f6165fd0226ecd58fc23
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2026-09-23 16:51:27 +0600

    updated Login

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, Navigate, useLocation, useNavigate } from "react-router-dom";
+import { Link, Navigate, useLocation, useNavigate } from "react-router";
 
 import { useAuth } from "../../context/AuthContext";
 import GoogleButton from "../../components/auth/GoogleButton";
