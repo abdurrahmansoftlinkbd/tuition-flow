1d9a133c945e9680bb3c452b3935cf59ff883bd9
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2026-09-23 16:46:13 +0600

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from "./pages/Home";
+
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+
 import DashboardLayout from "./layouts/DashboardLayout";
 import DashboardHome from "./pages/Dashboard/DashboardHome";
+
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 const router = createBrowserRouter([
   {
@@ -11,47 +17,62 @@ const router = createBrowserRouter([
   },
 
   {
-    path: "/dashboard",
-    element: <DashboardLayout />,
+    path: "/login",
+    element: <Login />,
+  },
+
+  {
+    path: "/register",
+    element: <Register />,
+  },
+
+  {
+    element: <ProtectedRoute />,
     children: [
       {
-        index: true,
-        element: <DashboardHome />,
-      },
+        path: "/dashboard",
+        element: <DashboardLayout />,
+        children: [
+          {
+            index: true,
+            element: <DashboardHome />,
+          },
 
-      {
-        path: "students",
-        element: <div className="text-2xl font-bold">Students Page</div>,
-      },
+          {
+            path: "students",
+            element: <div className="text-2xl font-bold">Students Page</div>,
+          },
 
-      {
-        path: "payments",
-        element: <div className="text-2xl font-bold">Payments Page</div>,
-      },
+          {
+            path: "payments",
+            element: <div className="text-2xl font-bold">Payments Page</div>,
+          },
 
-      {
-        path: "attendance",
-        element: <div className="text-2xl font-bold">Attendance Page</div>,
-      },
+          {
+            path: "attendance",
+            element: <div className="text-2xl font-bold">Attendance Page</div>,
+          },
 
-      {
-        path: "schedule",
-        element: <div className="text-2xl font-bold">Schedule Page</div>,
-      },
+          {
+            path: "schedule",
+            element: <div className="text-2xl font-bold">Schedule Page</div>,
+          },
 
-      {
-        path: "reports",
-        element: <div className="text-2xl font-bold">Reports Page</div>,
-      },
+          {
+            path: "reports",
+            element: <div className="text-2xl font-bold">Reports Page</div>,
+          },
 
-      {
-        path: "settings",
-        element: <div className="text-2xl font-bold">Settings Page</div>,
-      },
+          {
+            path: "profile",
+            element: <div className="text-2xl font-bold">Profile Page</div>,
+          },
 
-      {
-        path: "profile",
-        element: <div className="text-2xl font-bold">Profile Page</div>,
+          {
+            path: "settings",
+            element: <div className="text-2xl font-bold">Settings Page</div>,
+          },
+        ],
       },
     ],
   },
